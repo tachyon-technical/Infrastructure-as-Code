a450d8cd74bc6a9fcf3dcec5271a8fc30a2737bc
Shane <157341889+tachyon-technical@users.noreply.github.com>
2024-12-12 18:35:39 -0700

    Update nginx-stream-base.sh

--- cloud-init-nginx-streaming-base/nginx-stream-base.sh
+++ cloud-init-nginx-streaming-base/nginx-stream-base.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 
-BROTLI="https://github.com/google/brotli"
-ZLIB="https://www.zlib.net/current/zlib.tar.gz"
-ZSTD="https://github.com/facebook/zstd"
-PCRE2="https://github.com/PCRE2Project/pcre2"
-LIBATOMIC="https://github.com/ivmai/libatomic_ops"
-OPENSSL="https://www.openssl.org"
-NGINX="https://nginx.org"
-
-LOG_FILE="/tmp/cloudinit.log"
-ERR_FILE="/tmp/cloudinit_err.log"
+source /tmp/dep_lib.sh
 
 read OPENSSL_LATEST < <(wget -qO- --no-check-certificate $OPENSSL/source/ |
 	grep -Eo 'openssl-[A-Za-z0-9\.]+.tar.gz' | \
@@ -19,27 +10,10 @@ read NGINX_LATEST < <(wget -qO- --no-check-certificate $NGINX/download/ |
 	grep -Eo 'nginx-[A-Za-z0-9\.]+.tar.gz' | \
 	sort -V | tail -1 | sed -nre 's|^[^0-9]*(([0-9]+\.)*[A-Za-z0-9]+).tar.*|\1|p')
 
-read BROTLI_LATEST < <(cut -c5- <<< $(curl -s -L --insecure "$BROTLI/releases/latest" | \
-		grep -Eo "tag/v[0-9\.]+" | uniq))
-  
-read ZLIB_LATEST < <(curl -s -L --insecure "https://www.zlib.net/fossils/?C=M;O=D"  | \
-	    grep -Eo "zlib-[0-9\.]+" | sort -u -r | sed -n '1p' | \
-    	    sed -r 's|zlib-([0-9\.]+)\.|\1|')
-
-read ZSTD_LATEST < <(cut -c6- <<< $(curl -s -L --insecure "$ZSTD/releases/latest" | \
-                grep -Eo "tag/v[0-9\.]+" | uniq ))
-
-read PCRE2_LATEST < <(curl -s -L --insecure "$PCRE2/releases/latest" | \
-	     grep -o "pcre2-[0-9\.]\{3,\}" | \
-      	     grep -Eo "([0-9]{1,3}\.)[0-9]+" | uniq )
-
-read LIBATOMIC_LATEST < <(cut -c5- <<< $(curl -s -L --insecure "$LIBATOMIC/releases/latest" | \
-		grep -Eo "tag/v[0-9\.]+" | uniq )
-
 NGINX_CONFIG=$(
 	cat <<EOF
-./configure --prefix=/var/www/html \
---sbin-path=/usr/sbin/nginx \
+./configure --prefix=/opt/usr/share/\
+--sbin-path=$PREFIX/bin \
 --modules-path=/etc/nginx/modules \
 --conf-path=/etc/nginx/nginx.conf \
 --error-log-path=/var/log/nginx/error.log \
@@ -65,9 +39,25 @@ NGINX_CONFIG=$(
 --without-http_scgi_module \
 --without-http_grpc_module \
 --without-http_memcached_module \
---with-cc-opt="-O3 -march=native -funroll-loops -ffast-math -I /opt/usr/local/include/openssl/ \
-   -I /opt/usr/local/include/ -I /opt/usr/local/include/atomic_ops" \
---with-ld-opt="-L /opt/usr/local/lib -ldl -Wl,-rpath,/opt/usr/local/lib"
+--with-cc-opt="-O3 -march=native -funroll-loops -ffast-math -I $INCLUDE_DIR/openssl/ \
+   -I $INCLUDE_DIR/pcre2/ -I $INCLUDE_DIR/libatomic/ -I $INCLUDE_DIR/zlib/" \
+--with-ld-opt="-L $LIB_DIR -ldl -Wl,-rpath,$LIB_DIR"
+EOF
+)
+
+OPENSSL_CONF=$(
+    cat <<EOF
+./config \
+enable-ktls no-weak-ssl-ciphers no-ssl3 no-ssl3-method no-tls1 no-tls1_1 \
+no-idea no-psk no-srp no-srtp no-des no-rc2 no-rc4 no-rc5 no-md2 no-md4 no-mdc2  \
+no-legacy no-gost threads \
+enable-brotli --with-brotli-lib=$LIB_DIR --with-brotli-include=$INCLUDE_DIR/brotli \
+zlib-dynamic --with-zlib-lib=$LIB_DIR --with-zlib-include=$INCLUDE_DIR/zlib \
+enable-zstd-dynamic --with-zstd-lib=$LIB_DIR --with-zstd-include=$INCLUDE_DIR/zstd \
+--prefix=$PREFIX \
+--openssldir=$PREFIX/etc/openssl \
+--libdir=$LIB_DIR
+    
 EOF
 )
 
@@ -97,154 +87,81 @@ WantedBy=multi-user.target
 EOF
 )
 
-GCC_PROCS=1
-if [[ "$PROCS" -le 3 ]]; 
-  then GCC_PROCS=1; 
-elif [[ "$PROCS" -ge 4 ]] && [[ "$PROCS" -le 7 ]]; 
-  then GCC_PROCS=2; 
-elif [[ "$PROCS" -ge 8 ]] && [[ "$PROCS" -le 13 ]]; 
-  then GCC_PROCS=4; 
-else 
-  GCC_PROCS=6; fi
-
-function execute_and_log {
-	if
-		CMD_LINE=$2
-  		eval "$1" >/dev/null 2>>$ERR_FILE
-	then
-		echo -e "\tSuccess. [Line $CMD_LINE]" >>$LOG_FILE
-	else
-		echo -e "\tError. [Line $CMD_LINE]" >>$LOG_FILE
-	fi
-}
 
 echo "Running updates." >>$LOG_FILE
-execute_and_log "apt-get update -y" $LINENO
+execute_and_log "apt-get update -y" $0 $LINENO
 
 echo "Installing dependencies." >>$LOG_FILE
 execute_and_log "apt-get install -y git build-essential libpcre3 libpcre3-dev \
 	zlib1g zlib1g-dev libssl-dev libgd-dev libxml2 libxml2-dev \
-	uuid-dev ca-certificates" $LINENO
+	uuid-dev ca-certificates" $0 $LINENO
 
 echo "Changing to /tmp." >>$LOG_FILE
-execute_and_log "cd /tmp" $LINENO
-
-
-###### AUTOMATE THIS
-: '
-wget https://github.com/google/brotli/archive/refs/tags/v1.1.0.tar.gz
-tar xzf v1.1.0.tar.gz
-cd brotli-1.1.0/
-cmake -DCMAKE_INSTALL_PREFIX=/opt/usr/local -DCMAKE_LIBRARY_PATH=/opt/usr/local/lib64 -DCMAKE_BINARY_DIR=/opt/usr/local/bin -DCMAKE_CXX_FLAGS="-O3 -march=native -funroll-loops" -DCMAKE_C_FLAGS="-O3 -march=native -funroll-loops"
-make 
-make install
-
-wget   https://www.zlib.net/current/zlib.tar.gz
-tar xzf zlib.tar.gz
-cd zlib-1.3.1l
-setenv CFLAGS="-O3 -march=native -funroll-loops"
-
-./configure --prefix=/opt/usr/local --includedir=/opt/usr/local/include --libdir=/opt/usr/local/lib
-make 
-make -n install
-make install
-
-wget https://github.com/facebook/zstd/releases/download/v1.5.6/zstd-1.5.6.tar.gz
-tar xzf zstd-1.5.6.tar.gz
-cd zstd-1.5.6
-export INCLUDEDIR="/opt/usr/local/include"
-export LIBDIR="/opt/usr/local/lib"
-export PREFIX="/opt/usr/local/"
-make
-make install
-
-wget https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.44/pcre2-10.44.tar.bz2
-ar xvf pcre2-10.44.tar.bz2
-cd pcre2-10.44/
-
-./configure --prefix=/opt/usr/local --exec-prefix=/opt/usr/local --libdir=/opt/usr/local/lib --includedir=/opt/usr/local/include --enable-jit
-make -j
-make -n install
-make install
-
-https://github.com/ivmai/libatomic_ops/releases/download/v7.8.2/libatomic_ops-7.8.2.tar.gz
-tar xzf libatomic_ops-7.8.2.tar.gz
-cd libatomic_ops-7.8.2/
-
-./configure --prefix=/opt/usr/local --exec-prefix=/opt/usr/local --libdir=/opt/usr/local/lib --includedir=/opt/usr/local/include
-'
-
+execute_and_log "cd /tmp" $0 $LINENO
 
 echo "Downloading OpenSSL." >>$LOG_FILE
 execute_and_log "wget -qN ${OPENSSL}/source/openssl-${OPENSSL_LATEST}.tar.gz \
-                 -O /tmp/openssl-${OPENSSL_LATEST}.tar.gz" $LINENO
+                 -O /tmp/openssl-${OPENSSL_LATEST}.tar.gz" $0 $LINENO
 
 echo "Importing OpenSSL public key." >>$LOG_FILE
-execute_and_log "gpg --recv-keys BA5473A2B0587B07FB27CF2D216094DFD0CB81EF" $LINENO
+execute_and_log "gpg --recv-keys BA5473A2B0587B07FB27CF2D216094DFD0CB81EF" $0 $LINENO
 
 echo "Downloading OpenSSL digital signature." >>$LOG_FILE
 execute_and_log "wget -qN ${OPENSSL}/source/openssl-${OPENSSL_LATEST}.tar.gz.asc \
-  -O /tmp/openssl-${OPENSSL_LATEST}.tar.gz.asc" $LINENO
+  -O /tmp/openssl-${OPENSSL_LATEST}.tar.gz.asc" $0 $LINENO
 
 echo "Checking OpenSSL digital signature." >>$LOG_FILE
 execute_and_log "gpg --verify /tmp/openssl-${OPENSSL_LATEST}.tar.gz.asc \
-	/tmp/openssl-${OPENSSL_LATEST}.tar.gz" $LINENO
+	/tmp/openssl-${OPENSSL_LATEST}.tar.gz" $0 $LINENO
 
 echo "Downloading Nginx." >>$LOG_FILE
 execute_and_log "wget -qN ${NGINX}/download/nginx-${NGINX_LATEST}.tar.gz \
-	-O /tmp/nginx-${NGINX_LATEST}.tar.gz" $LINENO
+	-O /tmp/nginx-${NGINX_LATEST}.tar.gz" $0 $LINENO
 
 echo "Downloading Nginx public key." >>$LOG_FILE
 execute_and_log "wget -qN ${NGINX}/keys/nginx_signing.key \
-	-O /tmp/nginx_signing.key" $LINENO
+	-O /tmp/nginx_signing.key" $0 $LINENO
 
 echo "Importing Nginx public key." >>$LOG_FILE
-execute_and_log "gpg --quiet --import /tmp/nginx_signing.key" $LINENO
+execute_and_log "gpg --quiet --import /tmp/nginx_signing.key" $0 $LINENO
 
 echo "Downloading Nginx digital signature." >>$LOG_FILE
 execute_and_log "wget -qN ${NGINX}/download/nginx-${NGINX_LATEST}.tar.gz.asc \
-	-O /tmp/nginx-${NGINX_LATEST}.tar.gz.asc" $LINENO
+	-O /tmp/nginx-${NGINX_LATEST}.tar.gz.asc" $0 $LINENO
 
-# Derp verify digital signature
+echo "Checking Nginx digital signature." >>$LOG_FILE
+execute_and_log "gpg --verify /tmp/openssl-${NGINX_LATEST}.tar.gz.asc \
+	/tmp/nginx-${NGINX_LATEST}.tar.gz" $0 $LINENO
 
 echo "Extracting OpenSSL." >>$LOG_FILE
 execute_and_log "cd /tmp && tar -xvzf openssl-${OPENSSL_LATEST}.tar.gz && \
-	cd openssl-${OPENSSL_LATEST}" $LINENO
+	cd openssl-${OPENSSL_LATEST}" $0 $LINENO
 
 echo "Configuring OpenSSL." >>$LOG_FILE
-execute_and_log "./config \
-  enable-ktls no-weak-ssl-ciphers no-ssl3 no-ssl3-method no-tls1 no-tls1_1 \
-  no-idea no-psk no-srp no-srtp no-des no-rc2 no-rc4 no-rc5 no-md2 no-md4 no-mdc2  \
-  no-legacy no-gost threads \
-  enable-brotli --with-brotli-lib=/opt/usr/local/lib --with-brotli-include=/opt/usr/local/include/brotli \
-  zlib-dynamic --with-zlib-lib=/opt/usr/local/lib --with-zlib-include=/opt/usr/local/include \
-  enable-zstd-dynamic --with-zstd-lib=/opt/usr/local/lib --with-zstd-include=/opt/usr/local/include/\
-  --prefix=/opt/usr/local \
-  --openssldir=/opt/usr/local/openssl \
-  --libdir=/opt/usr/local/lib" $LINENO 
+execute_and_log "" $0 $LINENO 
 
 echo "Building OpenSSL." >>$LOG_FILE
-execute_and_log "make install_sw" $LINENO
+execute_and_log "make install_sw" $0 $LINENO
 
 #echo "Linking libraries." >>$LOG_FILE
-#execute_and_log "ldconfig /usr/lib64/" $LINENO
+#execute_and_log "ldconfig /usr/lib64/" $0 $LINENO
 
 echo "Extracting Nginx." >>$LOG_FILE
 execute_and_log "cd /tmp && tar -xvzf nginx-${NGINX_LATEST}.tar.gz && \
-	cd nginx-${NGINX_LATEST}" $LINENO
+	cd nginx-${NGINX_LATEST}" $0 $LINENO
 
 echo "Configuring Nginx." >>$LOG_FILE
-execute_and_log "$NGINX_CONFIG" $LINENO
+execute_and_log "$NGINX_CONFIG" $0 $LINENO
 
 echo "Building Nginx." >>$LOG_FILE
-execute_and_log "make -j$GCC_PROCS install" $LINENO
+execute_and_log "make -j$GCC_PROCS install" $0 $LINENO
 
 echo "Creating Nginx PID file" >>$LOG_FILE
 execute_and_log "sed -i -Ee 's|^#?pid.*$|pid  /etc/nginx/nginx.pid;|' \
   -e 's|^#?user.*$|user  www-data;|' /etc/nginx/nginx.conf && touch /etc/nginx/nginx.pid && \
-  chown www-data:www-data /etc/nginx/nginx.pid"  $LINENO
+  chown www-data:www-data /etc/nginx/nginx.pid"  $0 $LINENO
 
 echo "Configuring SystemD daemon."
 execute_and_log 'echo "$NGINX_SYSTEMD_UNIT" > /usr/lib/systemd/system/nginx.service && \
 	systemctl daemon-reload && \
-	systemctl start nginx.service' $LINENO
+	systemctl start nginx.service' $0 $LINENO
